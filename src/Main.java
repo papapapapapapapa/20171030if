39816daae5cf224c2cc0ca76b332add619e2ebb6
papapapapapapapa <r3386588@gmail.com>
2017-10-30 15:46:57 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) {
+	Scanner in=new Scanner(System.in);
+	int x;
+	x=in.nextInt();
+
+    if(x%2==0)
+        System.out.print("偶數");
+    if(x%2!=0)
+        System.out.print("奇數");
+
+
+
+
+
+    }
+}
